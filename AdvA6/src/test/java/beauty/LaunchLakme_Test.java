dd4687640349b53bb44510198b809a6340df87a4
sathish kumar <sathish kumar@SATHISH>
2024-05-31 15:24:07 +0530

    I have added a script "Launch lakmine" in beauty package.

---
+++ AdvA6/src/test/java/beauty/LaunchLakme_Test.java
@@ -0,0 +1,13 @@
+package beauty;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LaunchLakme_Test {
+	@Test
+	public void launchLakmine() {
+		Reporter.log("launch lakmine");
+		
+	}
+
+}
